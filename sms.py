910b5c6c2fb8b2a3d290604366019fccf08621a8
shipukhan2001 <149859698+shipukhan2001@users.noreply.github.com>
2024-01-23 19:50:01 +0600

    Update sms.py

--- sms.py
+++ sms.py
@@ -302,7 +302,7 @@ FACEBOOK  : Shipu Khan
 """)
   os.system("xdg-open https://www.facebook.com/profile.php?id=61555583623727")
   sys.exit()
-baner= f""" {baner} \033[1;37m
+baner= os.system("figlet -f slant Tounik")
 baner = str(baner)
 baner = baner.replace("0","")
 logo=f""" {baner} \033[1;37m
